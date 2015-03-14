d532005630e9857106b93ba0901e903441da28e1
Wiqich <sds_ysb@126.com>
2015-03-14 17:51:57 +0800

    new bin

--- bin/start-worker.sh
+++ bin/start-worker.sh
@@ -3,6 +3,6 @@ cur=`cd $(dirname $0); pwd`
 logs=$cur/../logs
 type=merger
 mkdir -p $logs
-nohup sh $cur/wharf $type> $logs/localhost-$(whoami)-${type}.log 2>&1 &
-sleep 2
-tail $logs/localhost-$(whoami)-${type}.log
+nohup sh $cur/wharf $type> $logs/localhost-$(whoami)-${type}.out 2>&1 &
+sleep 1
+tail $logs/localhost-$(whoami)-${type}.out
