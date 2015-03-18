ca6224befb24dce5c8baa3778221881120dc2451
Wiqich <bobo@bobo.local>
2015-03-18 17:26:05 +0800

    new v

---
+++ conf/wharf-env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export JAVA_HOME="/home/java7u67"
+export SCALA_HOME="/home/scala-2.11.5"
+
+export WHARF_HOME=`cd $(dirname $0)/../; pwd ;`
+export JAVA_HEAP="2048m"
+
+export WHARF_OPT=
