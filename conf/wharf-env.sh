5fe3a6c3159ea179c311c186e87804652c9a3d10
Wiqich <sds_ysb@126.com>
2015-03-23 21:09:18 +0800

    new version

--- conf/wharf-env.sh
+++ conf/wharf-env.sh
@@ -4,6 +4,6 @@ export JAVA_HOME="/home/java7u67"
 export SCALA_HOME="/home/scala-2.11.5"
 
 export WHARF_HOME=`cd $(dirname $0)/../; pwd ;`
-export JAVA_HEAP="2048m"
+export JAVA_HEAP="256m"
 
 export WHARF_OPT=
